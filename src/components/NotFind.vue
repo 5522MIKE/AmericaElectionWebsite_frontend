30478aa9d9aa8b94a5670ea6452c85dd74d6e2f3
1017705117 <48436640+1017705117@users.noreply.github.com>
2020-12-09 15:21:30 +0800

    fix:404页面美化

--- src/components/NotFind.vue
+++ src/components/NotFind.vue
@@ -1,5 +1,42 @@
 <template>
-    <div>
-        404
-    </div>
+    <div class="middle" data-status="404">
+		<div class="middle_content">
+            <p class="desc">
+                That page cannot be found!
+            </p>
+            <br><br>
+            <a href="/" class="btn btn-info btn-lg active" role="button">HomePage</a>
+        </div>
+	</div>
 </template>
+
+<style scoped>
+    .middle::before {
+    color: #ebeef2;
+    content: attr(data-status);
+    display: block;
+    font-size: 220px;
+    font-weight: 700;
+    left: 50%;
+    line-height: .75;
+    position: absolute;
+    top: 45%;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+    
+}
+.middle_content {
+    left: 50%;
+    line-height: .75;
+    position: absolute;
+    top: 45%;
+    -webkit-transform: translateX(-50%) translateY(-50%);
+    transform: translateX(-50%) translateY(-50%);
+}
+.desc {
+    font-family: 'BatangChe';
+    font-size: 45px;
+    font-weight: 700;
+    margin: 0;
+}
+</style>
